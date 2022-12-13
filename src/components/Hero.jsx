66b75bfc028af09d925913598b97a606c03bafac
Nikola Dojcinovic <goonergalactico@gmail.com>
2022-12-13 11:50:17 +0100

    finished desktop design

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import client1 from "../assets/client-databiz.svg";
+import client2 from "../assets/client-audiophile.svg";
+import client3 from "../assets/client-meet.svg";
+import client4 from "../assets/client-maker.svg";
+import heroImgDesktop from "../assets/image-hero-desktop.png";
+import heroImgMobile from "../assets/image-hero-mobile.png";
+
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <main className="p-[150px] flex">
+      <div className="left basis-1/2">
+        <h1 className="text-7xl font-[700]">
+          Make <br /> remote work
+        </h1>
+        <p className="text-gray py-10">
+          Get your team in sync, no matter your location. <br />
+          Streamline processes, create team rituals, and <br /> watch
+          productivity soar.{" "}
+        </p>
+        <button className="bg-black text-white py-3 px-6 rounded-xl cursor-pointer border border-black animate-bounce transition-all hover:bg-white hover:text-black">
+          Learn more
+        </button>
+        <div className="clients flex items-center space-x-10 mt-[100px]">
+          <img className="w-[100px] cursor-pointer transition-all duration-500 hover:scale-150" src={client1} alt="databiz" />
+          <img className="w-[100px] cursor-pointer transition-all duration-500 hover:scale-150" src={client2} alt="databiz" />
+          <img className="w-[100px] cursor-pointer transition-all duration-500 hover:scale-150" src={client3} alt="databiz" />
+          <img className="w-[100px] cursor-pointer transition-all duration-500 hover:scale-150" src={client4} alt="databiz" />
+        </div>
+      </div>
+      <div className="right basis-1/2 -mt-[150px] flex items-center justify-end">
+        <img className="h-[600px]" src={heroImgDesktop} alt="hero-image" />
+      </div>
+    </main>
+  );
+};
 
 export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,9 +12,39 @@ import close from "../assets/icon-close-menu.svg";
 
 const Navbar = () => {
   return (
-    <div>
-      <img src={close} alt="" />
-    </div>
+    <nav className="flex justify-between px-12 py-8">
+      <div className="left flex items-center">
+        <img
+          className="w-[100px] h-[30px] mr-[60px]"
+          src={logo}
+          alt="logo-image"
+        />
+        <ul className="hidden sm:flex space-x-10 text-gray transition-all hover:text-black">
+          <li>
+            <a href="#">
+              Features
+              <img className="inline ml-2" src={arrowDown} alt="arrow-down" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              Company
+              <img className="inline ml-2" src={arrowDown} alt="arrow-down" />
+            </a>
+          </li>
+          <li>
+            <a href="#">Careers</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+        </ul>
+      </div>
+      <div className="right flex text-gray space-x-10">
+        <p className="cursor-pointer py-2 transition-all hover:text-black">Login</p>
+        <button className="border border-black rounded-xl px-4 py-2 transition-all hover:bg-black hover:text-white">Register</button>
+      </div>
+    </nav>
   );
 };
 
