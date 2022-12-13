59c517233ec9acfa0ceb909dbd312a78875bf1e8
Nikola Dojcinovic <goonergalactico@gmail.com>
2022-12-13 10:45:26 +0100

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { features, company } from "../constants/index";
+import logo from "../assets/logo.svg";
+import arrowDown from "../assets/icon-arrow-down.svg";
+import arrowUp from "../assets/icon-arrow-up.svg";
+import todoIcon from "../assets/icon-todo.svg";
+import calendarIcon from "../assets/icon-calendar.svg";
+import remindersIcon from "../assets/icon-reminders.svg";
+import planningIcon from "../assets/icon-planning.svg";
+import menu from "../assets/icon-menu.svg";
+import close from "../assets/icon-close-menu.svg";
+
+const Navbar = () => {
+  return (
+    <div>
+      <img src={close} alt="" />
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/constants/index.js
@@ -0,0 +1,33 @@
+export const features = [
+    {
+        id: 1,
+        title: "Todo List"
+    },
+    {
+        id: 2,
+        title: "Calendar"
+    },
+    {
+        id: 3,
+        title: "Reminders"
+    },
+    {
+        id: 4,
+        title: "Planning"
+    },
+];
+
+export const company = [
+    {
+        id: 5,
+        title: "History",
+    },
+    {
+        id: 6,
+        title: "Our Team",
+    },
+    {
+        id: 7,
+        title: "Blog",
+    }
+];
